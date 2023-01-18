1541f7f386287c5da563cecd7c537314e9197c47
ZhengYu, Xu <zen-xu@outlook.com>
2023-01-18 13:30:17 +0800

    optimize window

--- src/window.rs
+++ src/window.rs
@@ -2,14 +2,21 @@ use std::collections::HashMap;
 use std::vec;
 
 use timely::dataflow::channels::pact::Pipeline;
-use timely::dataflow::operators::Operator;
+use timely::dataflow::operators::{FrontierNotificator, Operator};
 use timely::dataflow::{Scope, Stream};
 use timely::progress::{PathSummary, Timestamp};
 use timely::Data;
 
 pub trait Window<G: Scope, D: Data> {
-    fn give(&mut self, time: G::Timestamp, data: Vec<D>);
+    fn give(&mut self, time: G::Timestamp, datum: D) {
+        self.give_vec(time, vec![datum])
+    }
+    fn give_iterator<I: Iterator<Item = D>>(&mut self, time: G::Timestamp, iter: I) {
+        self.give_vec(time, iter.collect())
+    }
+    fn give_vec(&mut self, time: G::Timestamp, data: Vec<D>);
     fn try_emit(&mut self) -> Option<(G::Timestamp, Vec<(G::Timestamp, D)>)>;
+    fn drain(&mut self) -> Option<(G::Timestamp, Vec<(G::Timestamp, D)>)>;
 }
 
 pub trait WindowOp<G: Scope, D: Data> {
@@ -17,6 +24,7 @@ pub trait WindowOp<G: Scope, D: Data> {
         &self,
         name: &str,
         window: W,
+        drain_when_finished: bool,
     ) -> Stream<G, Vec<(G::Timestamp, D)>>;
 }
 
@@ -25,37 +33,58 @@ impl<G: Scope, D: Data> WindowOp<G, D> for Stream<G, D> {
         &self,
         name: &str,
         mut window: W,
+        drain_when_finished: bool,
     ) -> Stream<G, Vec<(G::Timestamp, D)>> {
         let mut stash = HashMap::new();
-        self.unary_notify(
-            Pipeline,
-            &name,
-            vec![],
-            move |input, output, notificator| {
-                input.for_each(|time, data| {
-                    let data = data.take();
-                    window.give(time.time().clone(), data);
+
+        self.unary_frontier(Pipeline, &name, |cap, _| {
+            let mut cap = Some(cap);
+            let mut notificator = FrontierNotificator::new();
+
+            move |input, output| {
+                if input.frontier().is_empty() {
+                    if drain_when_finished {
+                        if let Some((emit_time, emit_data)) = window.drain() {
+                            let cap = cap.as_mut().unwrap();
+                            let new_time = cap.delayed(&emit_time);
+                            stash
+                                .entry(new_time.clone())
+                                .or_insert_with(|| {
+                                    notificator.notify_at(new_time);
+                                    vec![]
+                                })
+                                .extend(emit_data)
+                        }
+                    }
+                    cap = None;
+                } else {
+                    input.for_each(|time, data| {
+                        window.give_vec(time.time().clone(), data.take());
+                    });
+
                     if let Some((emit_time, emit_data)) = window.try_emit() {
-                        let new_time = time.delayed(&emit_time);
+                        let cap = cap.as_mut().unwrap();
+                        let new_time = cap.delayed(&emit_time);
+                        cap.downgrade(&emit_time);
                         stash
-                            .entry(time.clone())
+                            .entry(new_time.clone())
                             .or_insert_with(|| {
                                 notificator.notify_at(new_time);
                                 vec![]
                             })
                             .extend(emit_data)
                     }
-                });
+                }
 
-                notificator.for_each(|time, _, _| {
+                notificator.for_each(&[input.frontier()], |time, _| {
                     if let Some(data) = stash.remove(&time) {
                         output.session(&time).give(data);
                     }
                 });
 
                 stash.retain(|_, v| !v.is_empty());
-            },
-        )
+            }
+        })
     }
 }
 
@@ -79,7 +108,7 @@ impl<T: Timestamp, D: Data> TumblingWindow<T, D> {
 }
 
 impl<G: Scope, D: Data> Window<G, D> for TumblingWindow<G::Timestamp, D> {
-    fn give(&mut self, time: <G>::Timestamp, data: Vec<D>) {
+    fn give_vec(&mut self, time: <G>::Timestamp, data: Vec<D>) {
         if self.emit_time.is_none() {
             self.emit_time = Some(self.size.results_in(&time).unwrap());
         }
@@ -122,6 +151,23 @@ impl<G: Scope, D: Data> Window<G, D> for TumblingWindow<G::Timestamp, D> {
         self.emit_time = Some(self.size.results_in(&emit_time).unwrap());
         Some((emit_time.clone(), data))
     }
+
+    fn drain(&mut self) -> Option<(<G>::Timestamp, Vec<(<G>::Timestamp, D)>)> {
+        let emit_time = self.emit_time.clone()?;
+        let mut reserved = vec![];
+        for (time, data) in self.buffer.drain() {
+            reserved.extend(
+                data.into_iter()
+                    .map(|v| (time.clone(), v))
+                    .collect::<Vec<_>>(),
+            );
+        }
+        if reserved.is_empty() {
+            return None;
+        }
+        reserved.sort_by(|(t1, _), (t2, _)| t1.cmp(&t2));
+        Some((emit_time, reserved))
+    }
 }
 
 #[cfg(test)]
@@ -138,7 +184,7 @@ mod tests {
                 let probe = stream.probe();
                 stream
                     .inspect_time(|t, v| println!("A {t:?} {v:?}"))
-                    .window("A", TumblingWindow::new(4, None))
+                    .window("A", TumblingWindow::new(4, None), false)
                     .inspect_time(|t, d| println!("B {t:?} {d:?}"));
                 (input, probe)
             });
